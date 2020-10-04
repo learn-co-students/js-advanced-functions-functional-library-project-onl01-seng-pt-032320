bee294c2c1b80d08795e46495a8d2885cf231d18
curtis <laurencechadwell@gmail.com>
2020-10-03 22:06:55 -0500

    Done.

--- index.js
+++ index.js
@@ -4,19 +4,33 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, value) {
+        let newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+
+        for (let i = 0; i < newCollection.length; i++){
+          value(newCollection[i])
+        }
+
+        return collection
+    },
+
+    map: function(collection,callback) {
+
+      let newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+
+      let newArr = newCollection.map(value => callback(value))
+      return newArr
 
     },
 
-    map: function() {
+    reduce: function(col = [], callback = () => {}, acc =0) {
+
+      let sum = collection.reduce(())
+
 
     },
 
-    reduce: function() {
-
-    },
-
-    functions: function() {
+    find: function() {
 
     },
 
