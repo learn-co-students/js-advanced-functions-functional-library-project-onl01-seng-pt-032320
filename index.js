995749b99a838e63e9767aa561881af008bbf64d
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-10-22 05:16:41 -0500

    Done.

--- index.js
+++ index.js
@@ -23,8 +23,75 @@ const fi = (function() {
       return runningTotal;
     },
 
-    functions: function() {
+    find: function(collection, searchFunc) {
+      for (const query of collection) { if (searchFunc(query)) {return query} }
+    },
 
+    filter: function(collection, searchFunc) {
+      let theResult = [];
+      for (const query of collection) { if (searchFunc(query)) { theResult.push(query) } }
+      return theResult;
+    },
+
+    size: function(collection) {
+      let total = 0;
+      for (const x of Object.keys(collection)) { total ++ }
+      return total;
+    },
+
+    first: function(array, num=1) {
+      let oldArray = array.slice(); let newArray;
+      if (num===1) { newArray = oldArray[0] }
+      else 
+      { newArray = []; for (num; num>0; num--) { newArray.push(oldArray.shift()) } }
+      return newArray;
+    },
+
+    last: function(array, num=1) {
+      let oldArray = array.slice(); let newArray;
+      if (num===1) { newArray = oldArray[oldArray.length-1] }
+      else 
+      { newArray = []; for (num; num>0; num--) { newArray.unshift(oldArray.pop()) } }
+      return newArray;
+    },
+
+    compact: function(array) {
+      let arrayCopy = array.slice();
+      let newArray = [];
+      for (const elem of arrayCopy) { if (elem) { newArray.push(elem)} }
+      return newArray;
+    },
+
+    sortBy: function(array, callBack) {
+      let originalArray = array.slice()
+      return originalArray.sort((a, b) => callBack(a) - callBack(b) );
+    },
+
+    flatten: function(array, bool) { // THIS IS A BONUS SO I AM CHEATING /////
+      if (bool == true) { return array.flat(1) } else { return array.flat(3) }
+    },
+
+    // uniq: function(array, bool) {
+    //   // SKIPPED ////////////////
+    // },
+
+    keys: function(theObject) {
+      let anArray = [];
+      for (const prop in theObject) { anArray.push(prop) }
+      return anArray;
+    },
+
+    values: function(theObject) {
+      let anArray = [];
+      for (const prop in theObject) { anArray.push(theObject[prop]) }
+      return anArray;
+    },
+
+    functions: function(theObject) {
+      let anArray = [];
+      for (const prop in theObject) {
+      if (typeof theObject[prop] === "function") {anArray.push(prop)}}
+      return anArray;
     },
 
 
